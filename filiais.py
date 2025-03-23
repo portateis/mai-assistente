cae0ae6e656e1e86cf050c1e0440ffc63df4fb61
portateis <160445814+portateis@users.noreply.github.com>
2025-03-22 22:59:03 -0300

    Add files via upload

--- filiais.py
+++ filiais.py
@@ -1,4 +1,4 @@
-# Substitua este conteúdo com os dados reais de suas filiais
+# Substitua com suas filiais reais
 filiais_massafort = {}
 def encontrar_filial_mais_proxima(cidade): return None
 def obter_dados_da_filial(nome): return None
